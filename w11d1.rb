d18eb43966000980cb46b5c87bad806be101c478
App Academy Student <student@appacademy.io>
2015-09-01 10:10:20 -0700

    finished rootnum & caesarcipher

--- w11d1.rb
+++ w11d1.rb
@@ -1,23 +1,42 @@
 require 'byebug'
 
-def rootnum(num)
-  while num >= 10
-    num = root_num_it(num)
+# def rootnum(num)
+#   while num >= 10
+#     num = root_num_it(num)
+#   end
+#   return num
+# end
+#
+# def root_num_it(num)
+#   root = 0
+#   while num > 0
+#     root += num % 10
+#     num /= 10
+#   end
+#   root
+# end
+#
+# p rootnum(12)
+# p rootnum(129)
+# p rootnum(367)
+# p rootnum(1285)
+# p rootnum(456897)
+
+
+def caesar_cipher(string, shift)
+  alphabet = ("a".."z").to_a
+  result = ""
+  string.each_char do |letter|
+    if letter == " "
+        result += " "
+        next
+    end
+    shift_num = alphabet.index(letter) + shift
+    shift_num -= 26 if shift_num >= 26
+    result += alphabet[shift_num]
   end
-  return num
+  result
 end
 
-def root_num_it(num)
-  root = 0
-  while num > 0
-    root += num % 10
-    num /= 10
-  end
-  root
-end
-
-p rootnum(12)
-p rootnum(129)
-p rootnum(367)
-p rootnum(1285)
-p rootnum(456897)
+p caesar_cipher("hello", 3)
+p caesar_cipher("my name is francesco", 2)
